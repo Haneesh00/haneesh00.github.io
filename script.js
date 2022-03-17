6672c625d06ce9e3e1fa755802e64eca2e7c77f1
Haneesh00 <96829268+Haneesh00@users.noreply.github.com>
2022-03-17 15:02:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=> {
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')) {
+        setTimeout(() => {
+            document.querySelector('.ham').style.display = 'inline';
+        }, 500);
+        document.querySelector('.cross').style.display = 'none';
+    }
+    else {
+        setTimeout(() => {
+            document.querySelector('.cross').style.display = 'inline';
+        }, 500); 
+        document.querySelector('.ham').style.display = 'none';
+    }
+});
